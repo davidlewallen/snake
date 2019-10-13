1011f7791b8cbccea385bfff0ac9f31a5c4ac94a
dlewallen <david.lewallen@endurance.com>
2019-10-13 11:30:46 -0600

    refactored most of file
    * Added ability to controll direction of ball moving
    * ball now moves larger jumps and slower, easier to control
    * ball will end game if it touches a wall
    * ball cannot move back on itself

--- src/index.js
+++ src/index.js
@@ -1,10 +1,97 @@
 document.body.onload = init;
 
-let height = 500;
-let width = 500;
-let nextX = 5;
-let nextY = 5;
-let goingDown = true;
+let height = 520;
+let width = 520;
+
+let nextX = 10;
+let nextY = 10;
+
+let previousDirection = "down";
+let direction = "down";
+let gameOver = false;
+
+function addEventsToDirectionalKeys() {
+  const keyMap = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40
+  };
+
+  document.body.addEventListener("keydown", event => {
+    const key = keyMap[event.keyCode];
+    const validKey = Boolean(key);
+
+    if (validKey) {
+      const differentDirection = key !== previousDirection;
+      const oppositeAxis =
+        (key === "left" && previousDirection !== "right") ||
+        (key === "right" && previousDirection !== "left") ||
+        (key === "up" && previousDirection !== "down") ||
+        (key === "down" && previousDirection !== "up");
+
+      if (differentDirection && oppositeAxis) {
+        direction = keyMap[event.keyCode];
+        previousDirection = direction;
+      }
+    }
+  });
+}
+
+function checkGameover() {
+  const hitTop = nextY < 10;
+  const hitRight = nextX >= width;
+  const hitBottom = nextY > height;
+  const hitLeft = nextX < 10;
+
+  if (hitTop || hitRight || hitBottom || hitLeft) {
+    gameOver = true;
+  }
+}
+
+function draw() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(nextX, nextY, 10, 0, 2 * Math.PI);
+  ctx.fill();
+
+  if (direction === "down") {
+    nextY += 20;
+  }
+  if (direction === "up") {
+    nextY -= 20;
+  }
+  if (direction === "left") {
+    nextX -= 20;
+  }
+  if (direction === "right") {
+    nextX += 20;
+  }
+
+  checkGameover();
+}
+
+function loop() {
+  let gameLoop = null;
+
+  gameLoop = setInterval(() => {
+    if (gameOver) {
+      console.error("Game Over");
+      return clearInterval(gameLoop);
+    }
+
+    draw();
+  }, 500);
+}
 
 function init() {
   const root = document.getElementById("root");
@@ -16,33 +103,7 @@ function init() {
 
   root.appendChild(canvasEl);
 
+  addEventsToDirectionalKeys();
+
   loop();
-}
-
-function draw() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-
-  ctx.clearRect(0, 0, 500, 500);
-
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(nextX, nextY, 5, 0, 2 * Math.PI);
-  ctx.fill();
-
-  if (nextY >= height - 5) {
-    goingDown = false;
-  }
-  if (nextY <= 5) {
-    goingDown = true;
-  }
-  if (goingDown === true) {
-    nextY = nextY + 2.0;
-  } else {
-    nextY = nextY - 2.0;
-  }
-}
-
-function loop() {
-  setInterval(draw, 10);
 }
