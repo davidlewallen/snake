8e137410d093ce0dbb88ff4c6949017b364d0d9b
davidlewallen <lewallen.david@gmail.com>
2019-10-16 15:08:26 -0600

    Large refactor * most of code has been refactored/moved around * tracking a list of units * fruit randomly gens on the board * Drawing logic rewritten, player and units are separate * unit should go to the unit's in front previousPosition * added logic to prevent going back on yourself with setDirection and requestedDirection

--- src/index.js
+++ src/index.js
@@ -3,23 +3,168 @@ document.body.onload = init;
 let height = 520;
 let width = 520;
 
-let nextX = 10;
-let nextY = 10;
+let unitList = [
+  {
+    currentPosition: [10, 10],
+    previousPosition: [10, 10]
+  }
+];
 
+let fruit = { currentPosition: [] };
+
+let currentDirection = "down";
 let previousDirection = "down";
-let direction = "down";
+let requestedDirection = "down";
+
 let gameOver = false;
+
+function getGameStatus() {
+  return unitList.some(unit => {
+    const [unitX, unitY] = unit.currentPosition;
+    const hitTop = unitY < 10;
+    const hitRight = unitX >= width;
+    const hitBottom = unitY > height;
+    const hitLeft = unitX < 10;
+
+    return hitTop || hitRight || hitBottom || hitLeft;
+  });
+}
+
+function drawUnit(ctx, x, y, isPlayer = false) {
+  ctx.fillStyle = isPlayer ? "green" : "red";
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function moveUnits(ctx) {
+  const [, ...units] = unitList;
+
+  units.forEach((unit, index) => {
+    const [nextX, nextY] = unit.currentPosition;
+
+    drawUnit(ctx, nextX, nextY);
+
+    updateUnitPosition(index + 1, ...unitList[index].previousPosition);
+  });
+}
+
+function updateUnitPosition(index, x, y) {
+  const unitListCopy = [...unitList];
+
+  unitListCopy[index].previousPosition = unitListCopy[index].currentPosition;
+
+  unitListCopy[index].currentPosition = [x, y];
+
+  unitList = unitListCopy;
+}
+
+function movePlayer(ctx) {
+  const [{ currentPosition }] = unitList;
+  const [x, y] = currentPosition;
+
+  drawUnit(ctx, x, y, true);
+
+  if (currentDirection === "down") {
+    updateUnitPosition(0, x, y + 20);
+  }
+  if (currentDirection === "up") {
+    updateUnitPosition(0, x, y - 20);
+  }
+  if (currentDirection === "left") {
+    updateUnitPosition(0, x - 20, y);
+  }
+  if (currentDirection === "right") {
+    updateUnitPosition(0, x + 20, y);
+  }
+}
+
+function drawUnits(ctx) {
+  movePlayer(ctx);
+
+  moveUnits(ctx);
+}
+
+function updateFruitPosition(x, y) {
+  fruit.currentPosition = [x, y];
+}
+
+function randomLocation() {
+  const randomNumber = Math.floor(Math.random() * (510 - 10) + 10);
+
+  const divisibleBy10 = randomNumber % 10 === 0;
+  const divisibleBy20 = (randomNumber - 10) % 20 === 0;
+
+  if (randomNumber === 10 || (divisibleBy10 && divisibleBy20)) {
+    return randomNumber;
+  }
+
+  return randomLocation();
+}
+
+function randomizeFruitLocation() {
+  const x = randomLocation();
+  const y = randomLocation();
+
+  const [mainUnit] = unitList;
+  const [currentX, currentY] = mainUnit.currentPosition;
+
+  const notWithin20pX = x + 20 < currentX || x - 20 > currentX;
+  const notWithin20pY = y + 20 < currentY || y - 20 > currentY;
+
+  if (notWithin20pX && notWithin20pY) {
+    updateFruitPosition(x, y);
+  } else {
+    randomizeFruitLocation();
+  }
+}
+
+function drawFruit(ctx) {
+  if (!fruit.currentPosition.length) {
+    randomizeFruitLocation();
+  }
+
+  const [currentX, currentY] = fruit.currentPosition;
+
+  ctx.fillStyle = "Blue";
+  ctx.beginPath();
+  ctx.arc(currentX, currentY, 10, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function setDirection() {
+  previousDirection = currentDirection;
+  currentDirection = requestedDirection;
+}
+
+function loop() {
+  let gameLoop = null;
+
+  gameLoop = setInterval(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    console.error("loop");
+
+    ctx.clearRect(0, 0, width, height);
+
+    drawFruit(ctx);
+
+    setDirection();
+
+    drawUnits(ctx);
+
+    if (getGameStatus()) {
+      return clearInterval(gameLoop);
+    }
+  }, 200);
+}
 
 function addEventsToDirectionalKeys() {
   const keyMap = {
     37: "left",
     38: "up",
     39: "right",
-    40: "down",
-    left: 37,
-    up: 38,
-    right: 39,
-    down: 40
+    40: "down"
   };
 
   document.body.addEventListener("keydown", event => {
@@ -35,62 +180,10 @@ function addEventsToDirectionalKeys() {
         (key === "down" && previousDirection !== "up");
 
       if (differentDirection && oppositeAxis) {
-        direction = keyMap[event.keyCode];
-        previousDirection = direction;
+        requestedDirection = keyMap[event.keyCode];
       }
     }
   });
-}
-
-function checkGameover() {
-  const hitTop = nextY < 10;
-  const hitRight = nextX >= width;
-  const hitBottom = nextY > height;
-  const hitLeft = nextX < 10;
-
-  if (hitTop || hitRight || hitBottom || hitLeft) {
-    gameOver = true;
-  }
-}
-
-function draw() {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-
-  ctx.clearRect(0, 0, width, height);
-
-  ctx.fillStyle = "red";
-  ctx.beginPath();
-  ctx.arc(nextX, nextY, 10, 0, 2 * Math.PI);
-  ctx.fill();
-
-  if (direction === "down") {
-    nextY += 20;
-  }
-  if (direction === "up") {
-    nextY -= 20;
-  }
-  if (direction === "left") {
-    nextX -= 20;
-  }
-  if (direction === "right") {
-    nextX += 20;
-  }
-
-  checkGameover();
-}
-
-function loop() {
-  let gameLoop = null;
-
-  gameLoop = setInterval(() => {
-    if (gameOver) {
-      console.error("Game Over");
-      return clearInterval(gameLoop);
-    }
-
-    draw();
-  }, 500);
 }
 
 function init() {
